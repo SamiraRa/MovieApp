20830af351e3d52966a259975a3341b3ff8c7470
SamiraRa <53578188+SamiraRa@users.noreply.github.com>
2022-03-27 13:49:24 +0600

    api call

--- src/Components/SearchMovies.js
+++ src/Components/SearchMovies.js
@@ -1,67 +1,78 @@
-import React, {useEffect, useState} from 'react';
+import React, {useState} from 'react';
 import { View, Text, Image, LogBox, ScrollView, TextInput } from 'react-native';
 import {POSTER_IMAGE} from '../config';
-import {GET} from '../API';
+import {API_URL} from '../API';
 import Loader from './Loader';
 import Styles from '../Styles';
 import axios from 'axios';
+import { TouchableHighlight } from 'react-native-web';
 
 
 const SearchMovies = () => {
     const [movies, setMovies] = useState([]);
     const[loading, setLoading] = useState(true);
-    const [state, setState] = useState('');
+    const [state, setState] = useState({
+      s:'' ,
+      results: [],
+      selected: {}
+    });
 
-    useEffect (()=>{
-    const getState = async () => {
-        const data = await GET (props.url);
-        setState(data.results);
-        setLoading(false);
-      };
-            getState();
-            }, []);
-  
-            const search  = () => {
-              if(data.results == {movieId: item.id}){
-                <FlatList
-                keyExtractor={item => item.id}
-                data={movies}
-                vertical
-                renderItem={item => displayMovies(item, props)}
-              />
-              }
-              else "No Movies Found";
+            const search = () => {
+              axios(API_URL+state.s).then(({data})=>{
+                let results = data.Search;
+                setState(prevState => {
+                  return {...prevState,results:results}
+                })
+              })
             }
-            const displayMovies = ({item}, props) => {
-              return (
-                <TouchableOpacity
-                  onPress={() => {
-                    props.navigation.push('movieDetails', {movieId: item.id});
-                  }}
-                  style={{marginHorizontal: 10}}>
-                  <Image
-                    source={{uri: `${POSTER_IMAGE}${item.poster_path}`}}
-                    style={Styles.posterImage}
-                  />
-                  <Text style={Styles.movieTitle}>{item.original_title}</Text>
-                </TouchableOpacity>
-              );
-            };
-            
+            const openPopup = (id) =>{
+              axios(API_URL+id).then(({data})=>{
+                let result = data;
+                console.log(result);
+                setState(prevState => {
+                  return{...prevState,selected: result};
+                });
+              });
+            }
   return (
-    <View>
+    <View >
  
     <TextInput style={Styles.searchbox} 
-    onChangeText ={ text => setState(prevState => text)}
+    onChangeText ={ text => setState(prevState => {
+      return {...prevState, s: text}
+    }) }
 
     onSubmitEditing ={search}
-    value={state}
+    value={state.s}
     />
+    <ScrollView>
+    {
+      state.results.map(result =>(
+
+        <TouchableHighlight listMode="SCROLLVIEW"
+        key={result.item.id}
+          onPress={() => {
+            open.popup('movieDetails', {movieId: item.id});
+          }}
+          style={{marginHorizontal: 10}}>
+          <Image
+            source={{uri: `${POSTER_IMAGE}${item.poster_path}`}}
+            style={Styles.posterImage}
+          />
+          <Text style={Styles.movieTitle}>{item.original_title}</Text>
+          
+        </TouchableHighlight>
+      ))}
+        </ScrollView>
+     
+   
 
   </View>
+
   );
 };
 
 
+  
 
 export default SearchMovies;

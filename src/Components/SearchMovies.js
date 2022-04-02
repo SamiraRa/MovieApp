136072308e2dd47ae39c8163e5e8be11ae65bc8f
SamiraRa <53578188+SamiraRa@users.noreply.github.com>
2022-04-02 11:18:12 +0600

    modifile file

--- src/Components/SearchMovies.js
+++ src/Components/SearchMovies.js
@@ -9,7 +9,8 @@ import Loader from './Loader';
 import Styles from '../Styles';
 import axios from 'axios';
 
-import { TouchableHighlight } from 'react-native-web';
+import { FlatList, TouchableHighlight } from 'react-native-web';
+import { TextField } from '@mui/material';
 
 
 const SearchMovies = () => {
@@ -19,7 +20,7 @@ const SearchMovies = () => {
 
             const Search = async () => {
               try{
-                const {data} = await axios.get('https://api.themoviedb.org/3/search/movie?2057ebea8436d7331724b6b9edcf1e78&languages=en-US&query=${searchText}&include_adult=false');
+                const {data} = await axios.get('https://api.themoviedb.org/3/search/movie?api_key=2057ebea8436d7331724b6b9edcf1e78&language=en-US&query=${searchText}&include_adult=false');
               setContent(data.results);         
               }catch(error){console.error(error);}};
               
@@ -33,13 +34,59 @@ const SearchMovies = () => {
             }, [])
           
   return (
-    <View >
-    <TextInput style={Styles.searchbox} 
+    <ScrollView nestedScrollEnabled={true} style={Styles.sectionBg}>
+    {loading ? (<Loader />) :
+    (
+    <View>
+    <TextInput
+    style={Styles.searchbox} 
     onChangeText ={ (e)=> setSearchText(e.target.value) }
     onSubmitEditing ={Search}
     />
+    <Button 
+    onClick={Search}
+    variant="contained"
+    style={{marginLeft:10}} >
+    <SearchIcon fontSize='large' />
+    </Button>
+  
     
-  </View>
+    { content &&
+      content.map((c)=>(
+      <FlatList 
+      key = {c.id}
+      id={c.id}
+      data={movies}
+      poster={c.poster_path}
+      title={c.title || title.name}
+      renderItem={c => displayMovies(item, props)}
+      />
+    ))};
+   
+    </View>
+    )}
+    </ScrollView>
+     ); 
+};
+const displayMovies = ({item}, props) => {
+  return (
+ 
+      
+    <TouchableOpacity
+      onPress={() => {
+        props.navigation.push('movieDetails', {movieId: c.id});
+      }}
+      style={{marginHorizontal: 10 , marginBottom: 20}}>
+      <View className='image-container d-flex justify-content-start m-3'>
+      <Image
+        source={{uri: `${POSTER_IMAGE}${item.poster_path}`}}
+        style={Styles.posterImage}
+        resizeMode= 'cover'
+      />
+      <View className='overlay d-flex align-items-center justify-content-center'></View>
+      </View>
+      <Text style={Styles.movieTitle}>{item.original_title}</Text>
+    </TouchableOpacity>
 
   );
 };
